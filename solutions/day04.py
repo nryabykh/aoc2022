b9828ab70453c6aef5dade55ba83babe9384ef82
Nikolai Riabykh <nryabykh@isgneuro.com>
2022-12-04 12:33:15 +0600

    simple beautfy

--- solutions/day04.py
+++ solutions/day04.py
@@ -12,13 +12,13 @@ def _get_bounds(s: str):
 
 class Solver(BaseSolver):
     def _prepare(self):
-        split = []
-        for line in self.data['input']:
-            first, second = line.split(',')
-            a, b = _get_bounds(first)  # a <= b
-            x, y = _get_bounds(second)  # x <= y
-            split.append((a, b, x, y))
-        self.data['split'] = split
+        self.data['split'] = [
+            (
+                *_get_bounds(line.split(',')[0]),
+                *_get_bounds(line.split(',')[1])
+            )
+            for line in self.data['input']
+        ]
 
     def _solve_one(self):
         return sum((a <= x and b >= y) or (x <= a and y >= b) for a, b, x, y in self.data['split'])
