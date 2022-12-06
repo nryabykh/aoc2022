998aca72f4ea933dd21965659eca19069632931e
Nikolai Riabykh <nryabykh@isgneuro.com>
2022-12-07 01:23:15 +0600

    add st secret

--- app/secrets.py
+++ app/secrets.py
@@ -1,5 +1,6 @@
 import os
+import streamlit as st
 
 
 def get_session_id():
-    return os.getenv('AOC_SESSION')
+    return st.secrets['AOC_SESSION'] if 'AOC_SESSION' in st.secrets else os.getenv('AOC_SESSION')
