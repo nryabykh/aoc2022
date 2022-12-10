2d4ed96781455040e8187a7edd056bf147c9897e
Nikolai Riabykh <nryabykh@isgneuro.com>
2022-12-10 21:37:04 +0600

    minor refactoring

--- solutions/day10.py
+++ solutions/day10.py
@@ -2,6 +2,7 @@
 --- Day 10: Cathode-Ray Tube ---
 https://adventofcode.com/2022/day/10
 """
+from typing import Iterable
 
 from common import BaseSolver
 
@@ -14,12 +15,15 @@ class Solver(BaseSolver):
     def _solve_one(self):
         cycle, strength, x = 1, 0, 1
         for n in self.data['actions']:
+            # check current register
             strength = self._update_strength(strength, cycle, x)
 
+            # if noop, just go to next tick
             if n == 0:
                 cycle += 1
                 continue
 
+            # if addX, check the next tick, jump over it, and update register
             strength = self._update_strength(strength, cycle + 1, x)
             cycle += 2
             x += n
@@ -30,27 +34,33 @@ class Solver(BaseSolver):
         cycle, x = 0, 1
         row, crt = "", []
         for n in self.data['actions']:
+            # if end of row, append it to crt and reset
             if len(row) >= 40:
                 crt.append(row[:40])
                 row = row[40:]
 
+            # update sprite and row
             sprite = (x - 1, x, x + 1)
-            pixel = '#' if cycle % 40 in sprite else '.'
-            row += pixel
+            row = self._update_row(row, sprite, cycle)
 
+            # if noop, go to the next tick
             if n == 0:
                 cycle += 1
                 continue
 
-            pixel = '#' if (cycle + 1) % 40 in sprite else '.'
-            row += pixel
+            # if addX, update row on the next tick, jump over the next tick, update register
+            row = self._update_row(row, sprite, cycle + 1)
             cycle += 2
             x += n
         crt.append(row)
 
-        crt = '\n' + '\n'.join(crt)
-        return crt
+        return '\n' + '\n'.join(crt)
 
     @staticmethod
     def _update_strength(strength, cycle, x):
         return strength + cycle * x * (cycle % 40 == 20)
+
+    @staticmethod
+    def _update_row(row: str, sprite: Iterable[int], cycle: int):
+        pixel = '#' if cycle % 40 in sprite else '.'
+        return row + pixel
